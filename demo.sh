b5f9d1f90cf4576e7c635620d2b9a02218f8d275
Tyler Rockwood <rockwood@redpanda.com>
2025-12-19 20:49:24 +0000

    initial commit

---
+++ demo.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# create topic, the property is `redpanda.kvstore=cloud|none`
+docker compose exec redpanda-0 /opt/redpanda/bin/rpk topic create example-topic -c 'redpanda.kvstore=cloud'
+echo "producing to topic"
+printf "foo bar\nhello world\nfoo qux\n" | docker compose exec --no-tty redpanda-0 /opt/redpanda/bin/rpk topic produce example-topic -f '%k %v\n'
+echo "consuming from topic"
+docker compose exec redpanda-0 /opt/redpanda/bin/rpk topic consume example-topic -o :end
+echo "doing kvstore lookup"
+docker compose exec redpanda-0 /opt/redpanda/bin/rpk kvstore get example-topic foo
+echo "doing kvstore scan"
+docker compose exec redpanda-0 /opt/redpanda/bin/rpk kvstore scan example-topic
